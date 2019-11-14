12e03bf663c429a3b69a9e6165f174848f82a4f4
PiotrRut <piotr.rutkowski@city.ac.uk>
2019-11-14 13:23:07 +0000

    Small changes/typos fix

--- src/pages/aboutme.js
+++ src/pages/aboutme.js
@@ -21,7 +21,7 @@ const AboutMe = (props) => (
                     <p>I am a 20 year old programmer and second year student at City, University of London, where I study BSc Computer Science. I was originally born in Poland, but for the majority of my life I've been living in Norway. In september 2018 I moved to London to study. Before I came to the UK, I graduated high school with a diploma in general studies, as well as electronical and electric engineering. </p>
                     <p>In my free time, apart from working on various programming projects, I dedicate a lot of time to photography. I've been taking pictures since I can remember, and the website you're currently on, doubles as my portfolio. Here you can browse the projects I've been working on, as well as some of the shots I've taken during the last months.</p>
                     <p>My passion from programming started a couple years ago, after accidentally finding a Python instruction course on YouTube. Ever since I have been trying to learn new languages, concepts, and technologies. What amazes me most about computer science, is how vast it really is, and how much you can do with even a slight knowledge of it.</p>
-                    <p>If you want to get to know me better, plase feel free to explore this website, and have a look at my recent blog posts, where I share whats I am up to and my opinions on various topics.</p>  
+                    <p>If you want to get to know me better, plase feel free to explore this website, and have a look at my recent blog posts, where I share whats I am up to and my opinions on various topics.</p>
                 </div>
             </section>
         </div>
--- src/pages/blog.js
+++ src/pages/blog.js
@@ -7,7 +7,7 @@ const Blog = () => (
         <section id="one">
             <div className="inner">
                 <h1>No blog posts just yet...</h1>
-                <p>First blog post coming soon! Stay tuned!</p>
+                <p>First blog post coming soon! Stay tuned</p>
             </div>
         </section>
     </div>
